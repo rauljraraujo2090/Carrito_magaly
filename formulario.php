9f9e15a8d95787610b90c20a38e6c2ee9d937731
MagalyTalavera <talaverasanchezmagaly@gmail.com>
2023-10-27 12:36:37 -0500

    r

--- formulario.php
+++ formulario.php
@@ -55,17 +55,18 @@ include("include/conexion.php");
                                     </div>   
                                     <div class="form-group row">
                                         <label class="col-lg-2 col-md-2 col-sm-12" >Foto:</label>
-                                        <input type="file" name="foto" class="form-control col-lg-4 col-md-4 col-sm-12" required>
+                                        <input type="file" name="foto" class="form-control col-lg-4 col-md-4 col-sm-12" required accept="image/*">
                                     </div>
                                     <div class="form-group row">
                                         <label class="col-lg-2 col-md-2 col-sm-12" >Rol:</label>
                                         <select name="rol" id="">
+                                        <option></option>
                                             <?php
                                             $b_roles = "SELECT * FROM roles";
                                             $r_b_roles = mysqli_query($conexion, $b_roles);
                                             while ($datos_roles = mysqli_fetch_array($r_b_roles)) {?>
                                                 <option value="<?php echo $datos_roles['id'];?>"><?php echo $datos_roles['nombre'];?></option>
-                                            <?}?>                                         
+                                            <?php }?>                                         
                                         </select>
                                     </div>    
 
--- operaciones/registrar_usuario.php
+++ operaciones/registrar_usuario.php
@@ -8,6 +8,30 @@ $correo=$_POST['correo'];
 $telefono=$_POST['telefono'];
 $direccion=$_POST['direccion'];
 $fecha_naci=$_POST['fecha_naci'];
+$id_rol=$_POST['rol'];
+
+
+
+$nombre_archivo = $dni.".jpg";
+$ruta_foto= "../img_usuarios/".$nombre_archivo;
+
+$pass_secure = password_hash($dni, PASSWORD_DEFAULT);
+
+
+if (move_uploaded_file($_FILES['foto']['tmp_name'], $ruta_foto)) {
+    $consulta="INSERT INTO usuario(dni, apellidos_nombres, correo, telefono, direccion, fecha_nacimiento, password, id_rol, foto, activo, reset_password, token_password)
+VALUES ('$dni', '$ape_nom', '$correo', '$telefono', '$direccion', '$fecha_naci', '$pass_secure','$id_rol','$nombre_archivo ',1,0,'')";
+
+$ejecutar= mysqli_query($conexion, $consulta);
+
+if ($ejecutar) {
+    echo "Registro Exitoso";
+}else {
+    echo "Registro Fallido";
+}
+}else {
+    echo "error al subir la foto";
+}
 
 //mostrar la informacion
 
@@ -18,15 +42,6 @@ $fecha_naci=$_POST['fecha_naci'];
 //echo $direccion."<br>";
 //echo $fecha_naci."<br>";
 
-$consulta="INSERT INTO usuario(dni, apellidos_nombres, correo, telefono, direccion, fecha_nacimiento, password, id_rol, foto, activo, reset_password, token_password)
-VALUES ('$dni', '$ape_nom', '$correo', '$telefono', '$direccion', '$fecha_naci', '$dni',1,'',1,0,'')";
 
-$ejecutar= mysqli_query($conexion, $consulta);
-
-if ($ejecutar) {
-    echo "Registro Exitoso";
-}else {
-    echo "Registro Fallido";
-}
 
 ?>
